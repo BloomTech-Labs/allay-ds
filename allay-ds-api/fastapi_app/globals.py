a39a8b0602f58ac745d66d6ef0fbd2b9682e14b1
jcs-lambda <57103874+jcs-lambda@users.noreply.github.com>
2020-05-12 13:58:03 -0500

    use spacy's sm NLP model

--- allay-ds-api/fastapi_app/globals.py
+++ allay-ds-api/fastapi_app/globals.py
@@ -2,11 +2,11 @@
 """
 
 import os
-import en_core_web_md
+import en_core_web_sm
 from tensorflow.keras.models import load_model
 
 # Initialize spaCy NLP model
-NLP = en_core_web_md.load()
+NLP = en_core_web_sm.load()
 
 # Load text classifier
 dir_path = os.path.dirname(os.path.realpath(__file__))
