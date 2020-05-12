e268af0604a65e9e3654dc9c37581477cf29ca02
jcs-lambda <57103874+jcs-lambda@users.noreply.github.com>
2020-05-12 10:09:54 -0500

    add global variable initialization

--- allay-ds-api/fastapi_app/__init__.py
+++ allay-ds-api/fastapi_app/__init__.py
@@ -1,5 +1,16 @@
 """Entry point for fastAPI application."""
 
+
+import en_core_web_md
+from tensorflow.keras.models import load_model
+
 from .app import create_app
 
+# Initialize FastAPI app
 APP = create_app()
+
+# Initialize spaCy NLP model
+NLP = en_core_web_md.load()
+
+# Load text classifier
+MODEL = load_model('model.h5')
